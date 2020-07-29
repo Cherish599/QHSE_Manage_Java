55e6a61b94379d059dfc33fd9a26d61c2de21413
zhuxin96 <314046573@qq.com>
2020-07-29 19:37:29 +0800

    ProblemDescription1

--- src/main/java/com/wlhse/controller/ProblemDescriptionController.java
+++ src/main/java/com/wlhse/controller/ProblemDescriptionController.java
@@ -16,26 +16,26 @@ public class ProblemDescriptionController {
     private ProblemDescriptionService problemDescriptionService;
 
     @RequestMapping(value = "/add_problemDescription", method = RequestMethod.POST,produces = {"application/json;charset=UTF-8"})
-    public R addProblemDescription(@RequestBody(required = false) ProblemDescriptionDto problemDescriptionDto)
+    public R addAccident(@RequestBody(required = false) ProblemDescriptionDto problemDescriptionDto)
     {
         return problemDescriptionService.addProblemDescription(problemDescriptionDto);
     }
 
     @RequestMapping(value = "/delete_problemDescription/{id}", method = RequestMethod.DELETE,produces = {"application/json;charset=UTF-8"})
-    public R deleteProblemDescription(@PathVariable int id)
+    public R deleteAccident(@PathVariable int id)
     {
         return problemDescriptionService.deleteProblemDescription(id);
     }
 
     @RequestMapping(value = "/update_problemDescription/{id}", method = RequestMethod.PUT,produces = {"application/json;charset=UTF-8"})
-    public R updateProblemDescription(@PathVariable int id,@RequestBody(required = false) ProblemDescriptionDto problemDescriptionDto)
+    public R updateAccident(@PathVariable int id,@RequestBody(required = false) ProblemDescriptionDto problemDescriptionDto)
     {
         problemDescriptionDto.setqHSE_AuditProblemRecord_ID(id);
         return problemDescriptionService.updateProblemDescription(problemDescriptionDto);
     }
 
     @RequestMapping(value = "/query_problemDescription", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public String queryProblemDescription(@ModelAttribute ProblemDescriptionDto problemDescriptionDto) {
+    public String queryAccident(@ModelAttribute ProblemDescriptionDto problemDescriptionDto) {
         return problemDescriptionService.queryProblemDescription(problemDescriptionDto);
     }
 }
