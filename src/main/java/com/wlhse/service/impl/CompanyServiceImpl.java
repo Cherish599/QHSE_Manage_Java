bd78b82359b795298cb2bf9924b1203569f86ff8
Benhkj1180@gmail.com <Va.4uCPWA_iY@aiLz4e->
2020-10-21 10:12:18 +0800

    Bug Fixed Commit By Coco 2020-10-21 10:12AM

--- src/main/java/com/wlhse/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/wlhse/service/impl/CompanyServiceImpl.java
@@ -1,17 +1,21 @@
 package com.wlhse.service.impl;
 
+import com.wlhse.cache.JedisClient;
 import com.wlhse.dao.CompanyDao;
 import com.wlhse.dto.outDto.CompanyOutDto;
+import com.wlhse.entity.CompanyPojo;
 import com.wlhse.service.CompanyService;
 import com.wlhse.util.R;
 import com.wlhse.util.TreeUtil;
-import com.wlhse.util.state_code.CodeDict;
 import com.wlhse.util.state_code.NR;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
@@ -21,6 +25,8 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Resource
     private TreeUtil treeUtil;
+    @Resource
+    JedisClient jedisClient;
 
     @Override
     public R listTreeCompany(Integer id) {
@@ -30,9 +36,27 @@ public class CompanyServiceImpl implements CompanyService {
     }
 
     @Override
-    public R listQhseTreeCompany() {
+    public R listQhseTreeCompany(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        String userId = "";
+        if (StringUtils.isNotBlank(token)) {
+            Map<String, String> map = jedisClient.hGetAll(token);
+            userId= map.get("userId");
+        }
+        List<CompanyPojo> companyPojos = companyDao.queryCompany(Integer.valueOf(userId));
+        //获取当前员工所在公司
+        CompanyPojo companyPojo = companyPojos.get(0);
         R ok = R.ok();
-        ok.put("data", treeUtil.getQhseCompanyTree(companyDao.queryQhseCompany()));
+        //职能部门
+        if (companyPojo.getCompanyCode().startsWith("0001")){
+
+            ok.put("data", treeUtil.getQhseCompanyTree(companyDao.queryQhseCompany()));
+        }
+        else {
+            //基层单位
+            ok.put("data",companyPojo);
+        }
         return ok;
     }
 
