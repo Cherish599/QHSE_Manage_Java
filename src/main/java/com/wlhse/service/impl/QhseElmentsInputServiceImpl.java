8779b14ebbe5bf103ecf8b50c29699e788c0daf0
swpu_lhl <1782707351@qq.com>
2020-10-28 10:36:25 +0800

    bug fix melon

--- src/main/java/com/wlhse/service/impl/QhseElmentsInputServiceImpl.java
+++ src/main/java/com/wlhse/service/impl/QhseElmentsInputServiceImpl.java
@@ -37,13 +37,14 @@ public class QhseElmentsInputServiceImpl implements QhseElementsInputService {
     public R addElementEvidenceAttach(ElementEvidenceAttachInDto elementEvidenceAttachInDto) {
         //首次录入数据
         //判断是不是不涉及
-        if(elementEvidenceAttachInDto.getEvidenceDescription().equals("录入判定该项要素不涉及流程，不予录入")){
-            elementEvidenceAttachInDto.setAttach("");
-            elementEvidenceAttachInDto.setAttachDescrption("录入判定该项要素不涉及流程，不予录入");
-            qhseElementsInputDao.add(elementEvidenceAttachInDto);
-            qhseElementsInputDao.addAttach(elementEvidenceAttachInDto);
-        }
-        else if (qhseElementsInputDao.query(elementEvidenceAttachInDto) == null) {
+//        if(elementEvidenceAttachInDto.getEvidenceDescription().equals("录入判定该项要素不涉及流程，不予录入")){
+//            elementEvidenceAttachInDto.setAttach("");
+//            elementEvidenceAttachInDto.setAttachDescrption("录入判定该项要素不涉及流程，不予录入");
+//            qhseElementsInputDao.add(elementEvidenceAttachInDto);
+//            qhseElementsInputDao.addAttach(elementEvidenceAttachInDto);
+//        }
+//        else
+        if (qhseElementsInputDao.query(elementEvidenceAttachInDto) == null) {
             qhseElementsInputDao.add(elementEvidenceAttachInDto);
             //将附件attach对应id放入elementFileInfo
             if(elementEvidenceAttachInDto.getAttach()!=null&&!"".equals(elementEvidenceAttachInDto.getAttach())) {
