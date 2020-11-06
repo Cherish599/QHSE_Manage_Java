87391ad676cb5726e13702887ee1d5f4fd71f257
tobing <652916578@qq.com>
2020-11-06 20:44:04 +0800

    Update Feature 更新FileUpload中隐患、位置图片上传的文件类型判断逻辑。 修复微信小程序中文件名与后缀名不清晰问题

--- src/main/java/com/wlhse/controller/FileUploadUtils.java
+++ src/main/java/com/wlhse/controller/FileUploadUtils.java
@@ -28,6 +28,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RequestMapping("/api/v3")
@@ -248,9 +249,9 @@ public class FileUploadUtils {
                 "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
             String originFileName = file.getOriginalFilename();
             String fileName = setFile(file, "resources\\QHSEDanger\\photoes");
-            drFileDao.insertNewFile(fileName,originFileName);
+            drFileDao.insertNewFile(fileName, originFileName);
             //拼接生成图片下载链接链接
-            String downloadLink="/downloadDangerFile?fileName="+fileName;
+            String downloadLink = "/downloadDangerFile?fileName=" + fileName;
             return NR.r(CodeDict.CODE_MESSAGE_DATA, 0, 0, downloadLink, null, 0, 0);
         } else {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_TYPE_ERROR, null, null, 0, 0);
@@ -267,29 +268,35 @@ public class FileUploadUtils {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_EMPTY, null, null, 0, 0);
         } else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
                 "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
-            String originFileName=file.getOriginalFilename();
+            String originFileName = file.getOriginalFilename();
             String fileName = setFile(file, "resources\\QualityDanger\\photoes");
-            drFileDao.insertNewFile(fileName,originFileName);
+            drFileDao.insertNewFile(fileName, originFileName);
             //拼接生成图片下载链接链接
-            String downloadLink="/downloadDangerFile?fileName="+fileName;
+            String downloadLink = "/downloadDangerFile?fileName=" + fileName;
             return NR.r(CodeDict.CODE_MESSAGE_DATA, 0, 0, downloadLink, null, 0, 0);
         } else {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_TYPE_ERROR, null, null, 0, 0);
         }
     }
 
+    private static final List<String> CONTENT_TYPES =
+            Arrays.asList("image/jpeg", "image/gif", "image/png", "image/bmp");
+
+    // TODO 微信小程序文件名会有异常
     @RequestMapping(value = "/uploadregulation", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
     @ResponseBody
     public String uploadregulation(@RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_EMPTY, null, null, 0, 0);
-        } else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
-                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
+        }
+//        else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
+//                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
+        else if (CONTENT_TYPES.contains(file.getContentType())) {
             String originFileName = file.getOriginalFilename();
             String fileName = setFile(file, "resources\\QHSERegulation\\photoes");
-            drFileDao.insertNewFile(fileName,originFileName);
+            drFileDao.insertNewFile(fileName, originFileName);
             //拼接生成图片下载链接链接
-            String downloadLink="/downloadRegulationFile?fileName="+fileName;
+            String downloadLink = "/downloadRegulationFile?fileName=" + fileName;
             return NR.r(CodeDict.CODE_MESSAGE_DATA, 0, 0, downloadLink, null, 0, 0);
         } else {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_TYPE_ERROR, null, null, 0, 0);
@@ -303,11 +310,13 @@ public class FileUploadUtils {
     public String qualityUploadregulation(@RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_EMPTY, null, null, 0, 0);
-        } else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
-                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
-            String originFileName=file.getOriginalFilename();
+        }
+//        else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
+//                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
+        else if (CONTENT_TYPES.contains(file.getContentType())) {
+            String originFileName = file.getOriginalFilename();
             String fileName = setFile(file, "resources\\QualityRegulation\\photoes");
-            drFileDao.insertNewFile(fileName,originFileName);
+            drFileDao.insertNewFile(fileName, originFileName);
             //拼接生成图片下载链接链接
             String downloadLink = "/downloadRegulationFile?fileName=" + fileName;
             return NR.r(CodeDict.CODE_MESSAGE_DATA, 0, 0, downloadLink, null, 0, 0);
@@ -316,14 +325,17 @@ public class FileUploadUtils {
         }
     }
 
+    // TODO 微信小程序文件名会有异常
     //上传远程监控截图,直接将截图下载链接放置于响应结果中
     @RequestMapping(value = "/uploadScreenShot", method = RequestMethod.POST)
     @ResponseBody
     public String uploadScreenShot(@RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             return NR.r(CodeDict.CODE_MESSAGE, -1, CodeDict.UPLOAD_EMPTY, null, null, 0, 0);
-        } else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
-                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
+        }
+//        else if ("jpg".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) || "png".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase()) ||
+//                "bmp".equals(file.getOriginalFilename().split("\\.")[1].toLowerCase())) {
+        else if (CONTENT_TYPES.contains(file.getContentType())) {
             String originFileName = file.getOriginalFilename();
             String fileName = setFile(file, "RemoteMonitor\\screenShot");
             monitorFileDao.insertNewFile(fileName, originFileName);
@@ -380,6 +392,7 @@ public class FileUploadUtils {
         }
         return NR.r(CodeDict.CODE_MESSAGE_DATA, 0, 0, lists, null, 0, 0);
     }
+
     @RequestMapping(value = "/Quality_evidence_upload", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
     @ResponseBody
     public String uploadQualityEvidence(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request) throws Exception {
